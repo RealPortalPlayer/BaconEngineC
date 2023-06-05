9751c884eb01b1a6934e187bccf242591f2191e2
portalplayer <email@portalplayer.xyz>
2023-06-05 16:27:28 -0400

    Package: Added trace logs

--- source/BaconEngine/Package.c
+++ source/BaconEngine/Package.c
@@ -1,12 +1,15 @@
 // Copyright (c) 2023, PortalPlayer <email@portalplayer.xyz>
 // Licensed under MIT <https://opensource.org/licenses/MIT>
 
+#include <SharedEngineCode/Logger.h>
+
 #include "BaconEngine/Package.h"
 #include "InterfaceFunctions.h"
 
 SEC_CPLUSPLUS_SUPPORT_GUARD_START()
 BE_Package BE_Package_Open(const char* fileName) {
 #ifndef BE_CLIENT_BINARY
+    SEC_LOGGER_TRACE("Opening package: %s\n", fileName);
     return zip_open(fileName, ZIP_DEFAULT_COMPRESSION_LEVEL, 'r');
 #else
     BE_INTERFACEFUNCTION(BE_Package, const char*);
@@ -16,6 +19,7 @@ BE_Package BE_Package_Open(const char* fileName) {
 
 SEC_Boolean BE_Package_GetFile(BE_Package package, const char* filePath, void** buffer, size_t* bufferSize) {
 #ifndef BE_CLIENT_BINARY
+    SEC_LOGGER_TRACE("Getting file in a package: %s\n", filePath);
     return zip_entry_open(package, filePath) == 0 &&
            zip_entry_read(package, buffer, bufferSize) >= 0 &&
            zip_entry_close(package) == 0;
@@ -27,6 +31,7 @@ SEC_Boolean BE_Package_GetFile(BE_Package package, const char* filePath, void** 
 
 void BE_Package_Close(BE_Package package) {
 #ifndef BE_CLIENT_BINARY
+    SEC_LOGGER_TRACE("Closing a package\n");
     zip_close(package);
 #else
     BE_INTERFACEFUNCTION(void, BE_Package)(package);
