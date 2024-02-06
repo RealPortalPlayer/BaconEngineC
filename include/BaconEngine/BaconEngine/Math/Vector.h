facad639df4fcd63f0f5b97344725febb51c2b6d
portalplayer <portalplayer@crime.su>
2024-02-06 08:55:38 -0500

    Vector: Simplified via macros

--- include/BaconEngine/BaconEngine/Math/Vector.h
+++ include/BaconEngine/BaconEngine/Math/Vector.h
@@ -1,65 +1,42 @@
 // Purpose: Vector structures.
 // Created on: 3/31/22 @ 1:55 PM
 
-// Copyright (c) 2022, 2023, PortalPlayer <email@portalplayer.xyz>
+// Copyright (c) 2022, 2023, 2024, PortalPlayer <email@portalplayer.xyz>
 // Licensed under MIT <https://opensource.org/licenses/MIT>
 
 #pragma once
 
 #include <BaconAPI/Internal/CPlusPlusSupport.h>
 
+#define BE_VECTOR_CREATE2(type, name) \
+typedef struct {                      \
+    type x;                           \
+    type y;                           \
+} BE_Vector2_ ## name
+
+#define BE_VECTOR_CREATE3(type, name) \
+typedef struct {                      \
+    type x;                           \
+    type y;                           \
+    type z;                           \
+} BE_Vector3_ ## name
+
+#define BE_VECTOR_CREATE4(type, name) \
+typedef struct {                      \
+    type x;                           \
+    type y;                           \
+    type z;                           \
+    type w;                           \
+} BE_Vector4_ ## name
+
 BA_CPLUSPLUS_SUPPORT_GUARD_START()
-typedef struct {
-    int x;
-    int y;
-} BE_Vector2_Integer;
-
-typedef struct {
-    unsigned x;
-    unsigned y;
-} BE_Vector2_Unsigned;
-
-typedef struct {
-    float x;
-    float y;
-} BE_Vector2_Float;
-
-typedef struct {
-    int x;
-    int y;
-    int z;
-} BE_Vector3_Integer;
-
-typedef struct {
-    unsigned x;
-    unsigned y;
-    unsigned z;
-} BE_Vector3_Unsigned;
-
-typedef struct {
-    float x;
-    float y;
-    float z;
-} BE_Vector3_Float;
-
-typedef struct {
-    int x;
-    int y;
-    int z;
-    int w;
-} BE_Vector4_Integer;
-
-typedef struct {
-    unsigned x;
-    unsigned y;
-    unsigned z;
-    unsigned w;
-} BE_Vector4_Unsigned;
-
-typedef struct {
-    float x;
-    float y;
-    float z;
-    float w;
-} BE_Vector4_Float;
+BE_VECTOR_CREATE2(int, Integer);
+BE_VECTOR_CREATE2(unsigned, Unsigned);
+BE_VECTOR_CREATE2(float, Float);
+BE_VECTOR_CREATE3(int, Integer);
+BE_VECTOR_CREATE3(unsigned, Unsigned);
+BE_VECTOR_CREATE3(float, Float);
+BE_VECTOR_CREATE4(int, Integer);
+BE_VECTOR_CREATE4(unsigned, Unsigned);
+BE_VECTOR_CREATE4(float, Float);
 BA_CPLUSPLUS_SUPPORT_GUARD_END()
