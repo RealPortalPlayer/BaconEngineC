9a5a64548522d116dc913789314350cb48bf63f3
portalplayer <portalplayer@crime.su>
2024-06-07 16:26:33 -0400

    Packets: Fixed it not using the engine/client interface correctly

--- source/BaconEngine/Server/Packets.c
+++ source/BaconEngine/Server/Packets.c
@@ -5,12 +5,24 @@
 
 #include "BaconEngine/Server/Packets.h"
 #include "BaconEngine/Server/Packet.h"
-#include "PrivatePacket.h"
+#include "../InterfaceFunctions.h"
+
+#ifndef BE_CLIENT_BINARY
+#   include "PrivatePacket.h"
+#endif
 
 void BE_Packets_SendPing(BE_Client client) {
+#ifndef BE_CLIENT_BINARY
     BE_Packet_Send(client, BE_PRIVATEPACKET_OPERATION_CODE_PING, NULL);
+#else
+    BE_INTERFACEFUNCTION(void, BE_Client)(client);
+#endif
 }
 
 void BE_Packets_SendConnect(void) {
+#ifndef BE_CLIENT_BINARY
     BE_Packet_Send(BE_CLIENT_UNCONNECTED, BE_PRIVATEPACKET_OPERATION_CODE_CONNECT, NULL);
+#else
+    BE_INTERFACEFUNCTION(void)();
+#endif
 }
